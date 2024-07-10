9771b689b659e6793de81d3ee8b4eb59e9ba2481
Eke Faith Chinonye <faithchinonye53@gmail.com>
2024-07-10 06:57:10 +0200

    refactor: Update import order in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./Component/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
